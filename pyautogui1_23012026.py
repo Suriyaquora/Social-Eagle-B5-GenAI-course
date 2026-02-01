f21d008dae157d8897f9802546e05bc9d7157963
Suriya Sudevan <102901589+Suriyaquora@users.noreply.github.com>
2026-02-01 12:34:27 +0530

    Add files via upload

---
+++ pyautogui1_23012026.py
@@ -0,0 +1,28 @@
+import pyautogui
+import time
+
+#Mouse operation
+pyautogui.rightClick(100,100)
+
+time.sleep(2)
+
+pyautogui.click(100,100)
+time.sleep(10)
+pyautogui.rightClick(2324,889)
+
+pyautogui.drag
+
+pyautogui.doubleClick(200,200)
+
+pyautogui.drag(100,100, duration=2)
+
+pyautogui.scroll(-500)
+
+#keyboard operation
+
+time.sleep(3)
+
+pyautogui.click(854,19)
+
+time.sleep(1)
+pyautogui.typewrite("this is my start")
